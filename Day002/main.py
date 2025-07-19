9a690f81900d99e20b9787b4de55b695907b55b5
Muras001 <walteroroma2022@gmail.com>
2025-07-19 15:06:13 +0300

    Initial Commit

---
+++ Day002/main.py
@@ -0,0 +1,11 @@
+#Prints Welcome Message
+print("Welcome to the tip calculator!!")
+
+#Takes bill, tip and no_people input from user
+bill = int(input("What is your total bill?: "))
+tip = int(input("How much tip would like to give? 10, 12, or 15?: "))
+no_people = int(input("How many people to split the bill?: "))
+
+#Calculates the amount payable per person and print the output
+bill_per_person = ((tip/100 + 1) * bill)/no_people
+print(f"Your bill per person is ${round(bill_per_person,2)}")
