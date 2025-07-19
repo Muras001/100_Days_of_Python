e3e44ea92a84643d02b2c97932a93a9566083ef9
Muras001 <walteroroma2022@gmail.com>
2025-07-19 15:40:43 +0300

    updated code

--- Day002/main.py
+++ Day002/main.py
@@ -9,3 +9,6 @@ no_people = int(input("How many people to split the bill?: "))
 #Calculates the amount payable per person and print the output
 bill_per_person = ((tip/100 + 1) * bill)/no_people
 print(f"Your bill per person is ${round(bill_per_person,2)}")
+
+#Acknowledgement
+print("Thank you for dinning with us!")
