208703bcdf0570094c87b94f418d317d447232e4
Muras001 <walteroroma2022@gmail.com>
2025-07-19 14:04:16 +0300

    Initial Commit

---
+++ Day001/main.py
@@ -0,0 +1,11 @@
+#Simple Band Name Generator Project
+
+#Prints Welcome Message
+print("Welcome To The Band Name Generator!!")
+
+#Takes input from user and store in the given variables
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+
+#Prints the concatenation of the city and pet variable as the name of the band
+print("your band name is: " + city + " " + pet)
